2061e5da2012edf0fde17ca65f52388636422b38
Raghu Betina <raghu@firstdraft.com>
2022-03-02 12:36:55 -0600

    Style countdown page

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -5,22 +5,22 @@ class BreaksController < ApplicationController
   def countdown
     @min_break_length = params[:break_length].to_i
 
-    @current_time = Time.now
+    @break_started_at = Time.now
 
     if params[:round_up] == "1"
-      @distance_to_next_five = (5 - (@current_time.min % 5))
+      @distance_to_next_five = (5 - (@break_started_at.min % 5))
 
       @distance_to_next_five = @distance_to_next_five == 5 ? 0 : @distance_to_next_five
-  
+
       @break_length = @distance_to_next_five.minutes + @min_break_length.minutes
     else
       @break_length = @min_break_length.minutes
     end
 
-    @break_end = @current_time + @break_length
-  
-    @break_end_without_seconds = @break_end.strftime("%I:%M%P")
-    
-    @break_end_for_timer = @break_end.strftime("%b %-d, %Y %H:%M:%S")
+    @break_ends_at = @break_started_at + @break_length
+
+    @break_ends_at_without_seconds = @break_ends_at.strftime("%I:%M%P")
+
+    @break_ends_at_for_timer = @break_ends_at.strftime("%b %-d, %Y %H:%M")
   end
 end
