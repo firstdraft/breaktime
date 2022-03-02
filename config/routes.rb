3979556d9569950b48ee1c165250315eaa79d629
Raghu Betina <raghu@firstdraft.com>
2022-03-01 18:55:23 -0600

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'breaks/new'
-  get 'breaks/countdown'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "breaks#new"
+  
+  get "countdown" => "breaks#countdown"
 end
