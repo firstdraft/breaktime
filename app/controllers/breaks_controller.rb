56d78f6b7631727cfe4d0301dba925689d017f6f
Raghu Betina <raghu@firstdraft.com>
2022-03-02 13:04:16 -0600

    Add time zones

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -3,24 +3,26 @@ class BreaksController < ApplicationController
   end
 
   def countdown
-    @min_break_length = params[:break_length].to_i
+    Time.use_zone(params[:time_zone]) do
+      @min_break_length = params[:break_length].to_i
 
-    @break_started_at = Time.now
+      @break_started_at = Time.current
 
-    if params[:round_up] == "1"
-      @distance_to_next_five = (5 - (@break_started_at.min % 5))
+      if params[:round_up] == "1"
+        @distance_to_next_five = (5 - (@break_started_at.min % 5))
 
-      @distance_to_next_five = @distance_to_next_five == 5 ? 0 : @distance_to_next_five
+        @distance_to_next_five = @distance_to_next_five == 5 ? 0 : @distance_to_next_five
 
-      @break_length = @distance_to_next_five.minutes + @min_break_length.minutes
-    else
-      @break_length = @min_break_length.minutes
+        @break_length = @distance_to_next_five.minutes + @min_break_length.minutes
+      else
+        @break_length = @min_break_length.minutes
+      end
+
+      @break_ends_at = @break_started_at + @break_length
+
+      @break_ends_at_without_seconds = @break_ends_at.strftime("%I:%M%P")
+
+      @break_ends_at_for_timer = @break_ends_at.strftime("%b %-d, %Y %H:%M")
     end
-
-    @break_ends_at = @break_started_at + @break_length
-
-    @break_ends_at_without_seconds = @break_ends_at.strftime("%I:%M%P")
-
-    @break_ends_at_for_timer = @break_ends_at.strftime("%b %-d, %Y %H:%M")
   end
 end
